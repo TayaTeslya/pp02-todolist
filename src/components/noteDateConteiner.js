b80ca9b4d062331555bb93dc0ee9c9ade363acdf
Taya Teslya <taya1teslya@gmail.com>
2022-06-12 19:31:35 +0300

    динамическое заполнение дат

--- src/components/noteDateConteiner.js
+++ src/components/noteDateConteiner.js
@@ -9,40 +9,38 @@ Date.prototype.addDays = function(days) {
     let date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     console.log(date.getDate() - 7);
-
-    if (date.getDate() - 7 < 0) {
-        date.setMonth(date.getMonth() + 1);
-    }
     return date;
 }
 
 
 function NoteDateConteiner() {
-    let mondayToday = new Date("2022-12-2");
-    let monday = new Date("2022-12-2");
-    // let mondayToday = new Date();
-    // let monday = new Date();
+    // let mondayToday = new Date("2022-10-2"); //26.09 - 2.10
+    // let monday = new Date("2022-10-2");
+    // let mondayToday = new Date("2022-06-28"); //28.02 - 03.07
+    // let monday = new Date("2022-06-28");
+    // let mondayToday = new Date("2022-02-28"); //28.02 - 6.03
+    // let monday = new Date("2022-02-28");
+    let mondayToday = new Date(); //6.06-12.06
+    let monday = new Date();
+    // const [monday, setMonday] = useState(mondayToday.getDate());
     monday.setDate(mondayToday.getDate() - (mondayToday.getDay() + 6) % 7);
-    // console.log(mondayToday.getDate() - monday.getDate());
-    if (mondayToday.getDate() - monday.getDate() < 0) {
-        monday.setMonth(monday.getMonth() - 1);
-    }
-    //const [monday, setMonday] = useState(mondayToday.getDate());
+
+    
 
   return (
     <div className="wrapper-conteiner">
         <div className="conteiner">
             <div className="note-date-conteiner">
                 <div className="note-date-weekday-conteiner"> {/* Будни */}
-                    <Weekday day="Пн" date={monday.addDays(0).getDate() + '' + (monday.addDays(0).getMonth() + 1 <= 9 ? '.0' + (monday.addDays(0).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
-                    <Weekday day="Вт" date={monday.addDays(1).getDate() + '' + (monday.addDays(1).getMonth() + 1 <= 9 ? '.0' + (monday.addDays(1).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
-                    <Weekday day="Ср" date={monday.addDays(2).getDate() + '' + (monday.addDays(2).getMonth() + 1 <= 9 ? '.0' + (monday.addDays(2).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
-                    <Weekday day="Чт" date={monday.addDays(3).getDate() + '' + (monday.addDays(3).getMonth() + 1 <= 9 ? '.0' + (monday.addDays(3).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
-                    <Weekday day="Пт" date={monday.addDays(4).getDate() + '' + (monday.addDays(4).getMonth() + 1 <= 9 ? '.0' + (monday.addDays(4).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
+                    <Weekday day="Пн" date={monday.getDate() + '.' + ((monday.getMonth()+1) <= 9 ? ('0' + (monday.getMonth()+1)) : (monday.getMonth()+1))}/>
+                    <Weekday day="Вт" date={monday.addDays(1).getDate() + '.' + ((monday.addDays(1).getMonth()+1) <= 9 ? ('0' + (monday.addDays(1).getMonth()+1)) : (monday.addDays(1).getMonth()+1))}/>
+                    <Weekday day="Ср" date={monday.addDays(2).getDate() + '.' + ((monday.addDays(2).getMonth()+1) <= 9 ? ('0' + (monday.addDays(2).getMonth()+1)) : (monday.addDays(2).getMonth()+1))}/>
+                    <Weekday day="Чт" date={monday.addDays(3).getDate() + '.' + ((monday.addDays(3).getMonth()+1) <= 9 ? ('0' + (monday.addDays(3).getMonth()+1)) : (monday.addDays(3).getMonth()+1))}/>
+                    <Weekday day="Пт" date={monday.addDays(4).getDate() + '.' + ((monday.addDays(4).getMonth()+1) <= 9 ? ('0' + (monday.addDays(4).getMonth()+1)) : (monday.addDays(4).getMonth()+1))}/>
                 </div>
                 <div className="note-date-weekends-conteiner"> {/* Выходные */}
-                    <Weekend day="Сб" date={monday.addDays(5).getDate() + '' + (monday.addDays(5).getMonth() <= 9 ? '.0' + (monday.addDays(5).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
-                    <Weekend day="Вс" date={monday.addDays(6).getDate() + '' + (monday.addDays(6).getMonth() <= 9 ? '.0' + (monday.addDays(6).getMonth() + 1) : '.' + (monday.addDays(6).getMonth() + 1))}/>
+                    <Weekend day="Сб" date={monday.addDays(5).getDate() + '.' + ((monday.addDays(5).getMonth()+1) <= 9 ? ('0' + (monday.addDays(5).getMonth()+1)) : (monday.addDays(5).getMonth()+1))}/>
+                    <Weekend day="Вс" date={monday.addDays(6).getDate() + '.' + ((monday.addDays(6).getMonth()+1) <= 9 ? ('0' + (monday.addDays(6).getMonth()+1)) : (monday.addDays(6).getMonth()+1))}/>
                 </div>
             </div>
         </div>
