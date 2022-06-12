f67d5200d4a8ee10238419b131ab74e241384685
Taya Teslya <taya1teslya@gmail.com>
2022-06-13 02:42:29 +0300

    удаление общих заметок

--- src/components/noteDateConteiner.js
+++ src/components/noteDateConteiner.js
@@ -13,11 +13,18 @@ Date.prototype.addDays = function(days) {
 
 
 function NoteDateConteiner(props) {
+    function changeReload(bool) {
+        setFirstColumn(firstColumn.filter(row => false));
+        setFirstColumn([]);
+        setReload(bool);
+    }
+    const [reload, setReload] = useState(false);
     let commonNotes = [];
     const [firstColumn, setFirstColumn] = useState([]);
     const [secondColumn, setSecondColumn] = useState([]);
     const [thirdColumn, setThirdColumn] = useState([]);
     useEffect(() => {
+        changeReload(false);
         fetch('http://localhost:3001/commonNotes').then((result) => { //по умолчанию get, поэтому его не указывают; fetch - промис
             return result.json(); //получаем данные из json, стали объектом js
         }).then((result) => { //тут уже используем объект js
@@ -47,42 +54,35 @@ function NoteDateConteiner(props) {
                         break;
                 }
             });
-            // console.log(first);
-            // console.log('-');
-            // console.log(second);
-            // console.log('-');
-            // console.log(third);
+            // console.log(firstColumn);
+            // console.log(secondColumn);
+            // console.log(thirdColumn);
             setFirstColumn(first);
             setSecondColumn(second);
             setThirdColumn(third);
-            // console.log(firstColumn);
-            // console.log('-');
-            // console.log(secondColumn);
-            // console.log('-');
-            // console.log(thirdColumn);
         }); 
-    }, []);
+    }, [reload]);
   return (
     <div className="wrapper-conteiner">
         <div className="conteiner">
             <div className="note-date-conteiner">
                 <div className="note-date-weekday-conteiner"> {/* Будни */}
-                    <Weekday day="Пн" date={props.weekdays.getDate() + '.' + ((props.weekdays.getMonth()+1) <= 9 ? ('0' + (props.weekdays.getMonth()+1)) : (props.weekdays.getMonth()+1))}/>
-                    <Weekday day="Вт" date={props.weekdays.addDays(1).getDate() + '.' + ((props.weekdays.addDays(1).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(1).getMonth()+1)) : (props.weekdays.addDays(1).getMonth()+1))}/>
-                    <Weekday day="Ср" date={props.weekdays.addDays(2).getDate() + '.' + ((props.weekdays.addDays(2).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(2).getMonth()+1)) : (props.weekdays.addDays(2).getMonth()+1))}/>
-                    <Weekday day="Чт" date={props.weekdays.addDays(3).getDate() + '.' + ((props.weekdays.addDays(3).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(3).getMonth()+1)) : (props.weekdays.addDays(3).getMonth()+1))}/>
-                    <Weekday day="Пт" date={props.weekdays.addDays(4).getDate() + '.' + ((props.weekdays.addDays(4).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(4).getMonth()+1)) : (props.weekdays.addDays(4).getMonth()+1))}/>
+                    <Weekday day="Пн" reload={reload} date={props.weekdays.getDate() + '.' + ((props.weekdays.getMonth()+1) <= 9 ? ('0' + (props.weekdays.getMonth()+1)) : (props.weekdays.getMonth()+1))}/>
+                    <Weekday day="Вт" reload={reload} date={props.weekdays.addDays(1).getDate() + '.' + ((props.weekdays.addDays(1).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(1).getMonth()+1)) : (props.weekdays.addDays(1).getMonth()+1))}/>
+                    <Weekday day="Ср" reload={reload} date={props.weekdays.addDays(2).getDate() + '.' + ((props.weekdays.addDays(2).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(2).getMonth()+1)) : (props.weekdays.addDays(2).getMonth()+1))}/>
+                    <Weekday day="Чт" reload={reload} date={props.weekdays.addDays(3).getDate() + '.' + ((props.weekdays.addDays(3).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(3).getMonth()+1)) : (props.weekdays.addDays(3).getMonth()+1))}/>
+                    <Weekday day="Пт" reload={reload} date={props.weekdays.addDays(4).getDate() + '.' + ((props.weekdays.addDays(4).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(4).getMonth()+1)) : (props.weekdays.addDays(4).getMonth()+1))}/>
                 </div>
                 <div className="note-date-weekends-conteiner"> {/* Выходные */}
-                    <Weekend day="Сб" date={props.weekdays.addDays(5).getDate() + '.' + ((props.weekdays.addDays(5).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(5).getMonth()+1)) : (props.weekdays.addDays(5).getMonth()+1))}/>
-                    <Weekend day="Вс" date={props.weekdays.addDays(6).getDate() + '.' + ((props.weekdays.addDays(6).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(6).getMonth()+1)) : (props.weekdays.addDays(6).getMonth()+1))}/>
+                    <Weekend day="Сб" reload={reload} date={props.weekdays.addDays(5).getDate() + '.' + ((props.weekdays.addDays(5).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(5).getMonth()+1)) : (props.weekdays.addDays(5).getMonth()+1))}/>
+                    <Weekend day="Вс" reload={reload} date={props.weekdays.addDays(6).getDate() + '.' + ((props.weekdays.addDays(6).getMonth()+1) <= 9 ? ('0' + (props.weekdays.addDays(6).getMonth()+1)) : (props.weekdays.addDays(6).getMonth()+1))}/>
                 </div>
             </div>
         </div>
         <div className="note-common-conteiner">
-            <Common notes={firstColumn}/>
-            <Common notes={secondColumn}/>
-            <Common notes={thirdColumn}/> 
+            <Common reload={changeReload} notes={firstColumn}/>
+            <Common reload={changeReload} notes={secondColumn}/>
+            <Common reload={changeReload} notes={thirdColumn}/> 
         </div>
     </div>
   );
--- src/components/notes/input.js
+++ src/components/notes/input.js
@@ -3,17 +3,28 @@ import { useEffect, useState } from 'react';
 
 
 function Input(props) {
-  const [note, setNote] = useState('');
-  useEffect(() => {
-    if (props.note['textCommonNote']) {
-      setNote(props.note['textCommonNote']);
+    function changeNote(event) {
+        if (event.target.value == '') { //если пустота, то удаляем запись в бд
+            fetch('http://localhost:3001/commonNotes?idCommonNote=' + props.note['idCommonNote'], {method: 'DELETE'}).then((err) => {
+                props.reload(true);
+            })
+        } else { //если что-то лежит в инпуте, изменяем запись в бд
+
+        }
     }
-  }, [props.note]);  
-  return (
-    <input value={(note != '') ? note : ''} onChange={((event) => {
-        setNote(event.target.value);
-    })}/> 
-  );
+    const [note, setNote] = useState('');
+    useEffect(() => {
+        if (props.note['textCommonNote']) {
+            setNote(props.note['textCommonNote']);
+        }
+    }, [props.note]);  
+    return (
+        <input value={(note != '') ? note : ''} onChange={((event) => {
+            setNote(event.target.value);
+        })} onBlur={((event) => {
+            changeNote(event);
+        })}/> 
+    );
 }
 
 export default Input;
